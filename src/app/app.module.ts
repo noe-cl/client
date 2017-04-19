a17e87ba15ce86fa7d23a956f9c5824a48e140f4
Supamiu <contact@flavien-normand.fr>
2017-04-19 21:06:07 +0200

    feat: added home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {Routes, RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
 
-import { AppComponent } from './app.component';
+const appRoutes: Routes = [
+    {
+        path: '', component: HomeComponent
+    }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        RouterModule.forRoot(appRoutes),
+
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
