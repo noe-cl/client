4d5771133c55bb4237c1d0f527a6cad3cedd13a1
Supamiu <contact@flavien-normand.fr>
2017-04-20 23:27:52 +0200

    style: updated the whole project to be scss-based.

--- src/app/progression/progression.component.ts
+++ src/app/progression/progression.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-progression',
     templateUrl: './progression.component.html',
-    styleUrls: ['./progression.component.css']
+    styleUrls: ['./progression.component.scss']
 })
 export class ProgressionComponent {
 
--- src/app/progression/status/status.component.ts
+++ src/app/progression/status/status.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
     selector: 'app-status',
     templateUrl: './status.component.html',
-    styleUrls: ['./status.component.css']
+    styleUrls: ['./status.component.scss']
 })
 export class StatusComponent {
 
